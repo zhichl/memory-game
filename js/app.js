e14d98498662864318e98b3c5938cd02326f27b6
zhichl <e.mbryo@hotmail.com>
2017-10-05 23:37:00 -0700

    Add Match Enum, chechMatch(), handleMatch() and moveCounter update method

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const MATCH_NUMBER = 2;
 const CARD_ICONS = ["fa-anchor", "fa-bolt", "fa-leaf", "fa-diamond", "fa-bomb", "fa-bicycle", "fa-paper-plane-o", "fa-cube"];
 const CARD_NUMBER = MATCH_NUMBER * CARD_ICONS.length;
+const log = console.log.bind(console);
 
 class Card {
 	constructor(icon, index) {
@@ -8,25 +9,40 @@ class Card {
 		this.faClasses = "fa " + icon;
 		this.index = index;
 	}
+
+	updateIndex(index) {
+		this.index = index;
+	}
+
 }
 
+// mimic Enum in Java / C++ for simple use and not type safe
+const Match = {
+	NOT_MATCH: "not match",
+	INCOMPLETE_MATCH: "incomplete match",
+	COMPLETE_MATCH: "complete match"
+};
+
+Object.freeze(Match);
+
 // list holding all cards
-var cards = initCards(CARD_ICONS),
+let cards = initCards(CARD_ICONS, MATCH_NUMBER),
 	clickStack = [],
 	matchStack = [],
 	moveCounter = 0,
 	stars = 3;
 
-// startGame();
+startGame();
 
 function startGame() {
-	cards = shuffle(cards);
+	// TODO: uncomment shuffle process
+	// cards = shuffle(cards);
 	clickStack = [];
 	matchStack = [];
 	moveCounter = 0;
 	stars = 3;
 	// TODO: dynamically create card HTML
-
+	createCardsHTML(cards);
 
 }
 
@@ -42,21 +58,27 @@ function endGame() {
  * @returns 
  */
 function initCards(icons, matchNumber) {
-	let cards = new Array();
+	let cards = [],
+		index = 0,
+		number = matchNumber;
+
 	for (let icon of icons) {
-		while (matchNumber-- > 0) {
-			let card = new Card(icon);
+		while (number-- > 0) {
+			let card = new Card(icon, index);
 			cards.push(card);
+			index++;
 		}
+		number = matchNumber;
 	}
 	return cards;
 }
 
-// Shuffle function, implementing Fisher–Yates shuffle algorithm
+// Shuffle function
+// shuffle the card array in place, implementing Fisher–Yates shuffle algorithm.
 function shuffle(array) {
 
 	// TODO: modify shuffle method
-	var currentIndex = array.length,
+	let currentIndex = array.length,
 		temporaryValue, randomIndex;
 
 	while (currentIndex !== 0) {
@@ -65,16 +87,21 @@ function shuffle(array) {
 		temporaryValue = array[currentIndex];
 		array[currentIndex] = array[randomIndex];
 		array[randomIndex] = temporaryValue;
+		array[currentIndex].updateIndex(currentIndex);
 	}
 
 	return array;
 }
 
 function createCardsHTML(cards) {
-	let $deck = $(".deck");
-	for(let card of cards) {
-		let $cardLi = $("<li>", {"class": "card"}),
-			$cardIcon = $("<i>", {"class": card.faClasses});
+	const $deck = $(".deck");
+	for (let card of cards) {
+		const $cardLi = $("<li>", {
+				"class": "card"
+			}),
+			$cardIcon = $("<i>", {
+				"class": card.faClasses
+			});
 
 		setIconAttr($cardIcon, card.faClasses);
 		addCardListener($cardLi);
@@ -82,7 +109,6 @@ function createCardsHTML(cards) {
 		$cardLi.append($cardIcon);
 		$deck.append($cardLi);
 	}
-
 }
 
 function setIconAttr($element, attr) {
@@ -102,66 +128,85 @@ function addCardListener($element) {
 
 		// if there're more than one card in the stack, check match and do something
 		if (clickStack.length > 1) {
-			let matched = checkMatch(clickStack);
-			// if cards are matched, 
-			if (matched) {
-				while (clickStack) {
-					let card = clickStack.pop();
-					let index = card.index;
-
-					matchStack.push(card);
-					$(".card").get(index).addClass("match");
-
-					//TODO: do something with matched card li
-
-				}
-
-				// check win
-				let won = checkWin();
-				if (won) {
-					//TODO: do something if won
-				}
-
-				// cards not matched
-			} else {
-
-				//TODO: do something with failing cards(change background) 
-				//      AND card li(hide icon)
-
-				moveCounter++;
-				// clear clickStack
-				clickStack = [];
-			}
-
+			let match = checkMatch(clickStack);
+			handleMatch(clickStack, match);
+			updateMoveCounter();
 			updateStars();
-
 		}
 
 	});
 }
 
-
-
 function checkMatch(cards) {
-	let matched = true;
+	let match = Match.INCOMPLETE_MATCH;
 	if (cards.length > 1) {
+		let matched = true;
 		for (let i = 1; i < cards.length && matched; i++) {
 			matched = (cards[i].icon === cards[i - 1].icon);
 		}
-	} else {
-		return false;
+		// not matched at all
+		if (!matched) {
+			return Match.NOT_MATCH;
+		}
 	}
 
-	// moveCounter increments by 1 if there's a complete match
-	if (matched && cards.length === MATCH_NUMBER) {
-		moveCounter++;
+	// if a incomplete match, further check if it's a complete match
+	if (match === Match.INCOMPLETE_MATCH && cards.length === MATCH_NUMBER) {
+		match = Match.COMPLETE_MATCH;
 	}
-	return matched;
+	return match;
+}
+
+function handleMatch(clickStack, match) {
+	while (clickStack.length > 0) {
+		const card = clickStack.pop();
+		const index = card.index;
+
+		// if cards are completely matched, 
+		if (match === Match.COMPLETE_MATCH) {
+			matchStack.push(card);
+			$(".card").eq(index).addClass("match");
+
+			//TODO: do something animating with matched card li
+
+			// check win
+			let won = checkWin();
+			if (won) {
+				//TODO: do something if won
+			}
+
+			// if cards are not matched
+		} else if (match === Match.NOT_MATCH) {
+
+			//TODO: do something with failing match(change background) 
+			//      AND card li(hide icon)
+
+			moveCounter++;
+			// clear clickStack
+			clickStack = [];
+
+		}
+		// (further work) if cards are incompletely matched(match === Match.INCOMPLETE_MATCH), do something
+
+
+
+	}
+
+
+
+
+
 }
 
 function updateStars() {
 	stars -= Math.floor(moveCounter / 8);
 	stars = Math.max(0, stars);
+}
+
+function updateMoveCounter(match) {
+	if (match === Match.COMPLETE_MATCH || match === Match.NOT_MATCH) {
+		moveCounter++;
+	}
 }
 
 function checkWin() {
