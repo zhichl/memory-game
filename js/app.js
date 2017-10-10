cd72e0d96ee8b345fc4e58cb2fa7b31d955d267e
zhichl <e.mbryo@hotmail.com>
2017-10-09 17:10:15 -0700

    Remove semicolons

--- js/app.js
+++ js/app.js
@@ -1,38 +1,38 @@
-const MATCH_NUMBER = 2;
-const CARD_ICONS = ["fa-anchor", "fa-bolt", "fa-leaf", "fa-diamond", "fa-bomb", "fa-bicycle", "fa-paper-plane-o", "fa-cube"];
-const PATTERN_NUMBER = CARD_ICONS.length;
-const CARD_NUMBER = MATCH_NUMBER * PATTERN_NUMBER;
-const STAR_NUMBER = 3;
-const FULL_STAR_ERROR_TOLERANCE = 4;
+const MATCH_NUMBER = 2
+const CARD_ICONS = ["fa-anchor", "fa-bolt", "fa-leaf", "fa-diamond", "fa-bomb", "fa-bicycle", "fa-paper-plane-o", "fa-cube"]
+const PATTERN_NUMBER = CARD_ICONS.length
+const CARD_NUMBER = MATCH_NUMBER * PATTERN_NUMBER
+const STAR_NUMBER = 3
+const FULL_STAR_ERROR_TOLERANCE = 4
 
 class Card {
 	constructor(icon, index) {
-		this.icon = icon;
-		this.faClasses = "fa " + icon;
-		this.index = index;
-		this.open = false;
-		this.matched = false;
+		this.icon = icon
+		this.faClasses = "fa " + icon
+		this.index = index
+		this.open = false
+		this.matched = false
 	}
 
 	updateIndex(index) {
-		this.index = index;
+		this.index = index
 	}
 
 	openCard() {
-		this.open = true;
+		this.open = true
 	}
 
 	closeCard() {
-		this.open = false;
+		this.open = false
 	}
 
 	matchCard() {
-		this.match = true;
+		this.match = true
 	}
 
 	resetState() {
-		this.open = false;
-		this.matched = false;
+		this.open = false
+		this.matched = false
 	}
 }
 
@@ -41,21 +41,21 @@ const Match = {
 	MISMATCH: "not matched",
 	INCOMPLETE_MATCH: "incomplete match",
 	COMPLETE_MATCH: "complete match"
-};
+}
 
 // prevent properties in Match from being modified
-Object.freeze(Match);
+Object.freeze(Match)
 
 //global variable declaration
 let cards, openStack, matchStack,
-	moveCounter, starCounter, timers;
+	moveCounter, starCounter, timers
 
 
 // init game, called only once
-initGame();
+initGame()
 
 // start game, called either restart button or play-again button is clicked
-startGame();
+startGame()
 
 function initGame() {
 	cards = createCards(CARD_ICONS, MATCH_NUMBER),
@@ -63,278 +63,278 @@ function initGame() {
 	matchStack = [],
 	moveCounter = 0,
 	starCounter = STAR_NUMBER,
-	timers = [];
-	addRestartButtonListener($(".score-panel .restart"));
-	addPlayAgainButtonListener($(".ending-modal button.play-again"));
+	timers = []
+	addRestartButtonListener($(".score-panel .restart"))
+	addPlayAgainButtonListener($(".ending-modal button.play-again"))
 }
 
 function startGame() {
-	shuffleCards();
+	shuffleCards()
 
 	// reset global variables / card stacks
-	resetGlobalVars();
+	resetGlobalVars()
 
 	// dynamically create / update card HTML
-	renderCards();
+	renderCards()
 
 	// hide ending modal and display game board
-	hideEndingModal();
-	displayGameBoard();
+	hideEndingModal()
+	displayGameBoard()
 
 	// set timer, game starts
-	startTimer(timers);
+	startTimer(timers)
 }
 
 function endGame() {
-	endTimer(timers);
-	updateStatsText();
+	endTimer(timers)
+	updateStatsText()
 	setTimeout(() => {
-		hideGameBoard();
-		displayEndingModal();
-	}, 500);
+		hideGameBoard()
+		displayEndingModal()
+	}, 500)
 }
 
 function resetGlobalVars() {
-	resetMatchStack();
-	resetOpenStack();
-	resetMoveCounter();
-	renderMoves();
-	resetStarCounter();
-	renderStars();
+	resetMatchStack()
+	resetOpenStack()
+	resetMoveCounter()
+	renderMoves()
+	resetStarCounter()
+	renderStars()
 }
 
 function displayGameBoard() {
-	$(".game-board").css("display", "flex");
+	$(".game-board").css("display", "flex")
 }
 
 function hideGameBoard() {
-	$(".game-board").css("display", "none");
+	$(".game-board").css("display", "none")
 }
 
 function displayEndingModal() {
-	$(".ending-modal").css("display", "flex");
+	$(".ending-modal").css("display", "flex")
 }
 
 function hideEndingModal() {
-	$(".ending-modal").css("display", "none");
+	$(".ending-modal").css("display", "none")
 }
 
 function startTimer(timers) {
-	timers[0] = new Date();
+	timers[0] = new Date()
 }
 
 function endTimer(timers) {
-	timers[1] = new Date();
+	timers[1] = new Date()
 }
 
 // unpdate info statistics
 function updateStatsText() {
 	// stars / moves / time
 	let starText = "stars",
-		moveText = "moves";
+		moveText = "moves"
 
 	const timeElapsed = timers[1] - timers[0],
-		timeText = formatMilliseconds(timeElapsed);
+		timeText = formatMilliseconds(timeElapsed)
 
 	if (moveCounter < 2 || starCounter < 2) {
 		if (moveCounter < 2) {
-			moveText = "move";
+			moveText = "move"
 		}
 		if (starCounter < 2) {
-			starText = "star";
+			starText = "star"
 		}
 	}
 
 	const msStats = `With ${moveCounter} ${moveText} and ${starCounter} ${starText}`,
-		timeStats = `Time: ${timeText}`;
-	$(".moves-stars").text(msStats);
-	$(".time").text(timeStats);
+		timeStats = `Time: ${timeText}`
+	$(".moves-stars").text(msStats)
+	$(".time").text(timeStats)
 }
 
 function addPlayAgainButtonListener($button) {
 	$button.on("click", () => {
-		startGame();
-	});
+		startGame()
+	})
 }
 
 // create new cards and return
 function createCards(icons, matchNumber) {
 	let cards = [],
 		index = 0,
-		number = matchNumber;
+		number = matchNumber
 
 	for (let icon of icons) {
 		while (number-- > 0) {
-			const card = new Card(icon, index);
-			cards.push(card);
-			index++;
+			const card = new Card(icon, index)
+			cards.push(card)
+			index++
 		}
-		number = matchNumber;
+		number = matchNumber
 	}
-	return cards;
+	return cards
 }
 
 function shuffleCards() {
-	cards = shuffle(cards);
+	cards = shuffle(cards)
 }
 
 // shuffle cards in place, implementing Fisher–Yates shuffle algorithm
 function shuffle(cards) {
 	let currentIndex = cards.length,
-		randomIndex;
+		randomIndex
 
 	while (currentIndex > 0) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
+		randomIndex = Math.floor(Math.random() * currentIndex)
+		currentIndex -= 1
 		// swap by destructuring
-		[cards[currentIndex], cards[randomIndex]] = [cards[randomIndex], cards[currentIndex]];
+		;[cards[currentIndex], cards[randomIndex]] = [cards[randomIndex], cards[currentIndex]]
 		// change card attrbute after shuffling
-		cards[currentIndex].updateIndex(currentIndex);
-		cards[currentIndex].resetState();
+		cards[currentIndex].updateIndex(currentIndex)
+		cards[currentIndex].resetState()
 	}
 
-	return cards;
+	return cards
 }
 
 function renderCards() {
-	deleteCardsHTML();
-	createCardsHTML(cards);
+	deleteCardsHTML()
+	createCardsHTML(cards)
 }
 
 function createCardsHTML(cards) {
-	const $deck = $(".deck");
+	const $deck = $(".deck")
 	for (let card of cards) {
 		const $card = $("<li>", {
 				"class": "card"
 			}),
 			$cardIcon = $("<i>", {
 				"class": card.faClasses
-			});
+			})
 
-		setIconAttr($cardIcon, card.faClasses);
-		addCardListener($card);
+		setIconAttr($cardIcon, card.faClasses)
+		addCardListener($card)
 
-		$card.append($cardIcon);
-		$deck.append($card);
+		$card.append($cardIcon)
+		$deck.append($card)
 	}
 }
 
 function deleteCardsHTML() {
-	$(".deck").empty();
+	$(".deck").empty()
 }
 
 function addRestartButtonListener($button) {
 	$button.on("click", () => {
-		startGame();
-	});
+		startGame()
+	})
 }
 
 function setIconAttr($element, attr) {
-	$element.addClass(attr);
+	$element.addClass(attr)
 }
 
 function addCardListener($element) {
 	$element.on("click", function () {
 		// open card and show the icon when clicked
-		$(this).addClass("open show");
+		$(this).addClass("open show")
 
 		// get corresponding card index
 		const cardIndex = $(this).index(),
-			card = cards[cardIndex];
+			card = cards[cardIndex]
 
 		// skip further actions if this card is already open or has been matched before click
 		if (!card.open && !card.matched) {
 			// add to open stack
-			card.openCard();
-			openStack.push(card);
+			card.openCard()
+			openStack.push(card)
 			// check match and update
-			const match = checkMatch(openStack);
-			renderMoves(match);
-			renderStars();
-			handleMatch(openStack, match);
+			const match = checkMatch(openStack)
+			renderMoves(match)
+			renderStars()
+			handleMatch(openStack, match)
 		}
-	});
+	})
 }
 
 function checkMatch(cards) {
-	let match = Match.INCOMPLETE_MATCH;
+	let match = Match.INCOMPLETE_MATCH
 	if (cards.length > 1) {
-		let matched = true;
+		let matched = true
 		for (let i = 1; i < cards.length && matched; i++) {
-			matched = (cards[i].icon === cards[i - 1].icon);
+			matched = (cards[i].icon === cards[i - 1].icon)
 		}
 		// not matched at all
 		if (!matched) {
-			return Match.MISMATCH;
+			return Match.MISMATCH
 		}
 	}
 
 	// if an incomplete match, further check if it's a complete match
 	if (match === Match.INCOMPLETE_MATCH && cards.length === MATCH_NUMBER) {
-		match = Match.COMPLETE_MATCH;
+		match = Match.COMPLETE_MATCH
 	}
-	return match;
+	return match
 }
 
 function handleMatch(openStack, match) {
-	const $cards = getNodeListFromCards(openStack);
+	const $cards = getNodeListFromCards(openStack)
 
 	// if cards are completely matched, 
 	if (match === Match.COMPLETE_MATCH) {
 		for (let card of openStack) {
-			card.matchCard();
+			card.matchCard()
 		}
 
 		// timeout to create simple matching effect
 		setTimeout(() => {
 			for (let $card of $cards) {
-				$card.addClass("match");
+				$card.addClass("match")
 				//TODO: (future work) do something animating with card matching
 			}
-		}, 300);
+		}, 300)
 
 		// if cards are not matched
 	} else if (match === Match.MISMATCH) {
 		for (let card of openStack) {
-			card.closeCard();
+			card.closeCard()
 		}
 
 		// timeouts to create simple dismatching effect
 		setTimeout(() => {
 			for (let $card of $cards) {
-				$card.addClass("match-fail");
+				$card.addClass("match-fail")
 			}
-		}, 300);
+		}, 300)
 
 		setTimeout(() => {
 			for (let $card of $cards) {
-				$card.removeClass("open show match-fail");
+				$card.removeClass("open show match-fail")
 			}
-		}, 600);
+		}, 600)
 		//TODO: (future work) do something animating with failing match
 	}
 	// TODO: (future work) if cards are incompletely matched(match === Match.INCOMPLETE_MATCH), do something
 
 	// check win
-	updateStacks(match);
-	const won = checkWin();
+	updateStacks(match)
+	const won = checkWin()
 	if (won) {
-		endGame();
+		endGame()
 	}
 }
 
 // render stars in the page according to the current star count
 function renderStars() {
-	updatestarCounter();
+	updatestarCounter()
 	if (starCounter < STAR_NUMBER) {
-		const $star = $(".score-panel .stars .fa").eq(starCounter);
+		const $star = $(".score-panel .stars .fa").eq(starCounter)
 		if ($star.hasClass("fa-star")) {
-			$star.removeClass("fa-star").addClass("fa-star-o");
+			$star.removeClass("fa-star").addClass("fa-star-o")
 		}
 		// all stars
 	} else {
-		const $stars = $(".score-panel .stars .fa");
-		$stars.removeClass("fa-star-o").addClass("fa-star");
+		const $stars = $(".score-panel .stars .fa")
+		$stars.removeClass("fa-star-o").addClass("fa-star")
 	}
 }
 
@@ -342,60 +342,60 @@ function renderStars() {
 // full stars when moves <= pattern-number + fullstar error tolerance  (12 moves)
 // decrease one star after then when every pattern-number moves added (8 moves)
 function updatestarCounter() {
-	starCounter = STAR_NUMBER - Math.floor((moveCounter - FULL_STAR_ERROR_TOLERANCE - 1) / PATTERN_NUMBER);
-	starCounter = Math.max(0, starCounter);
+	starCounter = STAR_NUMBER - Math.floor((moveCounter - FULL_STAR_ERROR_TOLERANCE - 1) / PATTERN_NUMBER)
+	starCounter = Math.max(0, starCounter)
 }
 
 function renderMoves(match) {
-	updateMoveCounter(match);
-	$(".moves").text(moveCounter);
+	updateMoveCounter(match)
+	$(".moves").text(moveCounter)
 }
 
 // one move is added as a complete match or a mismatch is presented
 function updateMoveCounter(match) {
 	if (match === Match.COMPLETE_MATCH || match === Match.MISMATCH) {
-		moveCounter++;
+		moveCounter++
 	}
 }
 
 function resetMatchStack() {
-	matchStack = [];
+	matchStack = []
 }
 
 function resetOpenStack() {
-	openStack = [];
+	openStack = []
 }
 
 function resetMoveCounter() {
-	moveCounter = 0;
+	moveCounter = 0
 }
 
 function resetStarCounter() {
-	starCounter = STAR_NUMBER;
+	starCounter = STAR_NUMBER
 }
 
 function getNodeListFromCards(cards) {
-	let $cards = [];
+	let $cards = []
 	for (let card of cards) {
-		let $card = getNodeFromCard(card);
-		$cards.push($card);
+		let $card = getNodeFromCard(card)
+		$cards.push($card)
 	}
-	return $cards;
+	return $cards
 }
 
 function getNodeFromCard(card) {
-	return $(".card").eq(card.index);
+	return $(".card").eq(card.index)
 }
 
 function updateStacks(match) {
-	updateMatchStack(match);
-	updateOpenStack(match);
+	updateMatchStack(match)
+	updateOpenStack(match)
 }
 
 // empty open stack when cards inside are completely matched or not matched
 function updateOpenStack(match) {
 	if (match === Match.COMPLETE_MATCH || match === Match.MISMATCH) {
-		resetOpenStack();
+		resetOpenStack()
 	}
 }
 
@@ -403,48 +403,48 @@ function updateOpenStack(match) {
 function updateMatchStack(match) {
 	if (match === Match.COMPLETE_MATCH) {
 		for (let card of openStack) {
-			matchStack.push(card);
+			matchStack.push(card)
 		}
 	}
 }
 
 function checkWin() {
-	return matchStack.length === CARD_NUMBER;
+	return matchStack.length === CARD_NUMBER
 }
 
 // format ms to readable time string
 function formatMilliseconds(time) {
-	time = Math.floor(time / 1000);
-	const h = Math.floor(time / 3600);
-	time %= 3600;
-	const m = Math.floor(time / 60);
-	const s = time % 60;
+	time = Math.floor(time / 1000)
+	const h = Math.floor(time / 3600)
+	time %= 3600
+	const m = Math.floor(time / 60)
+	const s = time % 60
 	let formattedTime,
 		hText = `${m}m `,
 		mText = `${m}m `,
-		sText = `${s}s`;
+		sText = `${s}s`
 
 	if (h === 0) {
-		hText = "";
+		hText = ""
 		// h === 0 && m === 0
 		if (m === 0) {
-			mText = "";
+			mText = ""
 			// h === 0 && m > 0
 		} else {
 			if (0 < s && s < 10) {
-				sText = "0" + sText;
+				sText = "0" + sText
 			}
 		}
 		// h > 0
 	} else {
 		if (0 < m && m < 10) {
-			mText = "0" + mText;
+			mText = "0" + mText
 		}
 		if (0 < s && s < 10) {
-			sText = "0" + sText;
+			sText = "0" + sText
 		}
 	}
-	formattedTime = hText + mText + sText;
+	formattedTime = hText + mText + sText
 
-	return formattedTime;
+	return formattedTime
 }
